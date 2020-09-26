3f67dcf4008ec8b35a32fcd6f0ede7ed2e05b11a
Rishi <Rishi@Apples-MacBook-Pro.local>
2020-09-26 20:06:42 +0530

    Added Composition component

--- ng-app/src/app/pages/composition/composition.component.ts
+++ ng-app/src/app/pages/composition/composition.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { Subscription } from "rxjs";
+import { CompositionEditComponent } from "./composition-edit/composition-edit.component";
+import { CompositionService } from "./composition.service";
 
 @Component({
-  selector: 'app-composition',
-  templateUrl: './composition.component.html',
-  styleUrls: ['./composition.component.scss']
+  selector: "app-composition",
+  templateUrl: "./composition.component.html",
+  styleUrls: ["./composition.component.scss"],
 })
-export class CompositionComponent implements OnInit {
+export class CompositionComponent implements OnInit, OnDestroy {
+  compositions: any[];
+  reloadEmitterSubscription: Subscription;
 
-  constructor() { }
+  constructor(
+    private compositionServ: CompositionService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
+    this.loadAll();
+    this.reloadEmitterSubscription = this.compositionServ.reloadEmitter.subscribe(
+      () => {
+        this.loadAll();
+      }
+    );
   }
 
+  ngOnDestroy(): void {
+    this.reloadEmitterSubscription.unsubscribe();
+  }
+
+  loadAll() {
+    this.compositionServ.getAllCompositions().subscribe((res) => {
+      this.compositions = res.body as any[];
+    });
+  }
+
+  open(composition?: any) {
+    const modalRef = this.modalService.open(CompositionEditComponent, {
+      size: "sm",
+      scrollable: true,
+      centered: false,
+      backdrop: "static",
+    });
+    modalRef.componentInstance.composition = Object.assign({}, composition);
+  }
 }
